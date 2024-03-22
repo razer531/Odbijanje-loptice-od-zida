119cdd484bd31c56f97e5e10411a73c8d68cabe9
Lovro Glavina <backyard.mig@gmail.com>
2024-03-22 01:39:21 +0100

    change

--- main.py
+++ main.py
@@ -2,50 +2,83 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from matplotlib.patches import Circle
-from math import ceil, sin, cos, radians
+from math import ceil, sin, cos, radians, degrees, sqrt, atan
+
+k = 0.7
 
 #Kreiramo klasu lopta 
 
 class Lopta:
+    global k
+    
     def __init__(self, x0, y0, v0, theta, radijus):
-        self.koord = x0, y0
+        self.x, self.y = x0, y0
         self.theta = theta #pocetni kut nagiba
         self.v = v0
         self.radijus = radijus
-
-lopta = Lopta(radijus = 0.5, x0 = 0, y0 = 5, v0 = 2, theta = 0)
-
-
-def kreiraj_putanju(x0, y0, v0, theta, num_it, delta_t):
+        self.x_lista = [self.x] 
+        self.y_lista = [self.y]
+        self.lupio = False
+        
+    def kreiraj_putanju(self):
 
     #Funkcija koja nam stvara x i y koordinate za loptu tijekom putanje, inicijaliziranu početnim
     #uvjetima: x0, y0, v0, theta(kut početnog izbačaja u stupnjevima), num_it(broj iteracija), delta_t.
+    #Vrijednosti x i y, kao i vy, računate su Eulerovom metodom. vx posebno ne računamo jer je ono 
+    #konstatno za danu putanju.
+        
 
-    vx0, vy0 = v0*cos(radians(theta)), v0*sin(radians(theta))
+        global delta_t, x_zid
+
+        vx0, vy0 = self.v*cos(radians(self.theta)), self.v*sin(radians(self.theta))
         
-    x_lista = [x0]
-    for i in range(num_it):
-        x_lista.append(x_lista[i] + delta_t*vx0)
-    
-    vy_lista = [vy0]
-    for i in range(num_it):
-        vy_lista.append(vy_lista[i] + delta_t*(-9.81))
+        vy_lista = [vy0]
         
-    y_lista = [y0]
-    for i in range(num_it):
-        y_lista.append(y_lista[i] + delta_t*vy_lista[i])
-    
-    return x_lista, y_lista
+        while self.v > 0.5:
+
+            while self.x <= x_zid and self.y >= 0:
+
+                self.x_lista.append(self.x_lista[-1] + delta_t*vx0)
+                vy_lista.append(vy_lista[-1] + delta_t*(-9.81))
+                self.y_lista.append(self.y_lista[-1] + delta_t*vy_lista[-1])
+                
+                self.x, self.y = self.x_lista[-1], self.y_lista[-1]
+                
+            
+                
+            vx0 = -k*vx0
+            vy0 = -k*vy_lista[-1]
+            for i in range(1000):
+                self.x_lista.append(self.x_lista[-1] + delta_t*vx0)
+                vy_lista.append(vy_lista[-1] + delta_t*(-9.81))
+                self.y_lista.append(self.y_lista[-1] + delta_t*vy_lista[-1])
+                
+                self.x, self.y = self.x_lista[-1], self.y_lista[-1]
+
+            break
+
+
+           
+
+        return self.x_lista, self.y_lista
+
+
+         
         
-    
+
+
+
+lopta = Lopta(radijus = 0.5, x0 = 0, y0 = 2, v0 = 10, theta = 45)
 
 
 fig, ax = plt.subplots()
-ax.set_xlim(-1, 6)
-ax.set_ylim(0, 7)
+ax.set_xlim(0, 10)
+ax.set_ylim(0, 10)
 
 #Crtamo zid
-ax.plot([5,5], [0,7], color = "black")
+x_zid = 9
+y_zid = 9
+ax.plot([x_zid, x_zid], [0,y_zid], color = "black", linewidth = 5)
 
 '''
 #Crtamo loptu na početku
@@ -58,29 +91,25 @@ krug = Circle((lopta.koord[0], lopta.koord[1]), radius = lopta.radijus, edgecolo
 ax.add_patch(krug) 
 ax.set_aspect('equal', adjustable='box')
 '''
-#Inicijaliziramo početne parametre, s time da t_sudar treba točno izračunati u ovisnosti o prethodnim 
-#parametrima, jer tada želimo zaustaviti kreiranje prvog dijela putanje.
-delta_t = 0.02
-h = 1
-vx0 = 5
-vy0 = 3
+
+#Inicijaliziramo početne parametre
+delta_t = 0.015
+
 t_start = 0
+t_end = 2
+num_it = ceil((t_end-t_start)/delta_t)
 
-#Sada je potrebno odabrati t_end pažljivo u ovisnosti o vx0, vy0, tj. da bu
-t_sudar = 2
-num_it = ceil((t_sudar-t_start)/delta_t)
+x,y = lopta.kreiraj_putanju()
 
-x, y = kreiraj_putanju(0, h, 7, 45, num_it, delta_t)
 putanja, = ax.plot(x[0], y[0], '-o', color = "red", markersize = 0.5)
 
-def update(num_it):
-    
+def update(frame):
     global putanja
-
-    novi_x = x[:num_it]
-    novi_y = y[:num_it]
-
+    
+    novi_x = x[:frame]
+    novi_y = y[:frame]
     putanja.set_data(novi_x, novi_y)
+
     
 
 animation = FuncAnimation(fig, update, frames=range(num_it), interval=1)
