a4a1f0c06793ef36d74d46ca60730bc248a04fb7
razer531 <backyard.mig@gmail.com>
2024-03-24 17:42:38 +0100

    Zamijeni main.py as alt.py

--- main.py
+++ main.py
@@ -27,162 +27,98 @@ class Lopta:
         self.theta = theta0 #kut nagiba
         self.v = v0
         self.greska = 0 #odstupanje loptice od početne visine kad se vrati na x = 0
+        self.povijest = {}
+       
+        self.povijest["x_pov"] = [x0]
+        self.povijest["y_pov"] = [y0]
+        self.povijest["v_pov"] = [v0]
+        self.povijest["theta_pov"] = [theta0]
+        
 
-        
-    def kreiraj_putanju(self):
+    def update(self):
+        vx, vy = self.v*cos(radians(self.theta)), self.v*sin(radians(self.theta))
 
-    #Funkcija koja nam mijenja x, y, v, theta za loptu tijekom putanje sve do sudara i vraća koordinate tijekom putanje      
-    #uvjetima: x0, y0, v0, theta(kut početnog izbačaja u stupnjevima), delta_t.
-    #Vrijednosti x i y, kao i vy, računate su Eulerovom metodom. vx posebno ne računamo jer je ono 
-    #konstatno za danu putanju.
-
-        
-        
-        
-        vx0, vy0 = self.v*cos(radians(self.theta)), self.v*sin(radians(self.theta))
-        
-        vy_lista = [vy0]
-        x_lista, y_lista  = [self.x], [self.y]
-        
-        while True:
-
-            if self.x <= x_zid and self.x >= 0 and self.y >= 0.1:
-                
-                vy_lista.append(vy_lista[-1] + delta_t*(-9.81))
-
-                x_lista.append(x_lista[-1] + delta_t*vx0)
-                y_lista.append(y_lista[-1] + delta_t*vy_lista[-1])
-                
-                self.x, self.y = x_lista[-1], y_lista[-1]
-                continue
+        if self.x <= x_zid-0.2 and self.x >= 0 and self.y >= 0.3:
             
-            elif self.x > x_zid:
-                x_lista.pop()
-                y_lista.pop()
-                self.x, self.y = x_lista[-1], y_lista[-1]
-                vy = vy_lista[-1]
-                vx = -k*vx0
-                self.v = sqrt(vy**2 + vx**2)
-                self.theta = calc_theta(vx, vy)
-                break
-            
-            elif self.y < 0.1:
-                x_lista.pop()
-                y_lista.pop()
-                self.x, self.y = x_lista[-1], y_lista[-1]
-                vy = -k*vy_lista[-1]
-                vx = vx0
-                self.v = sqrt(vy**2 + vx**2)
-                self.theta = calc_theta(vx, vy)
-                break
+            self.x += delta_t*vx
+            self.y += delta_t*vy
+            vx = vx
+            vy += delta_t * (-9.81)
+            self.v = sqrt(vy**2 + vx**2)
+            self.theta = calc_theta(vx, vy)
 
-            else:
-                break
+            self.povijest["x_pov"].append(self.x)
+            self.povijest["y_pov"].append(self.y)
+            self.povijest["v_pov"].append(self.v)
+            self.povijest["theta_pov"].append(self.theta)
+        
+        elif self.x > x_zid-0.2:
+            vx = -k*vx
+            self.x += delta_t*vx
+            self.y += delta_t*vy
+            self.v = sqrt(vy**2 + vx**2)
+            self.theta = calc_theta(vx, vy)
+
+            self.povijest["x_pov"].append(self.x)
+            self.povijest["y_pov"].append(self.y)
+            self.povijest["v_pov"].append(self.v)
+            self.povijest["theta_pov"].append(self.theta)
                 
-        return x_lista, y_lista
+        elif self.y < 0.3:
+            vy = -k*vy
+            self.x += delta_t*vx
+            self.y += 2*delta_t*vy
+            self.v = sqrt(vy**2 + vx**2)
+            self.theta = calc_theta(vx, vy)
 
-    def simulacija(self):
-        x_lista, y_lista = [], []
+            self.povijest["x_pov"].append(self.x)
+            self.povijest["y_pov"].append(self.y)
+            self.povijest["v_pov"].append(self.v)
+            self.povijest["theta_pov"].append(self.theta)
+            
+    def simulacija(self, animiraj = False):
         
-        while self.x >=0 and self.v > 1:
-            x_c, y_c = self.kreiraj_putanju()
-            x_lista.extend(x_c)
-            y_lista.extend(y_c)
-
-        self.greska = y_lista[-1] - y_lista[0]
-        return x_lista, y_lista 
-
-         
-
-def animacija_vise_loptica(): #n je broj loptica
-    
-    print()
-    n = int(input("Odaberite broj loptica: "))
-    
-    lopte = []
-    for i in range(n):
-        v = float(input(f"Početna brzina {i+1}. loptice: "))
-        theta = float(input(f"Početni kut nagiba {i+1}. loptice: "))
-        print()
-        lopta = Lopta(x0 = 0, y0 = 2, v0 = v, theta0 = theta)
-        lopte.append(lopta)
-
-    fig, ax = plt.subplots()
-    ax.set_xlim(0, 20)
-    ax.set_ylim(0, 10)
-
-    #Crtamo zid
-
-    ax.plot([x_zid, x_zid], [0,y_zid], color = "black", linewidth = 5)
-
-    putanje = []
-    x = []
-    y = []
-
-    for i in range(n):
-        x_c, y_c = lopte[i].simulacija()
-        x.append(x_c)
-        y.append(y_c)
-
-        putanja, = ax.plot(x[i][0], y[i][0], '-o', color = "red", markersize = 5) 
-        putanje.append(putanja)
-
-    def update(frame):
-        for i in range(n):
-            putanje[i].set_data(x[i][frame], y[i][frame])
+        while self.v > 1 and self.x >= 0:
+            self.update()
         
-    num_it = max([len(t) for t in x])
-    animation = FuncAnimation(fig, update, frames=range(num_it), interval=1)
+        self.greska = self.y - self.povijest["y_pov"][0]
 
-    plt.show()
+        if animiraj:
+            
+            x, y = self.povijest["x_pov"], self.povijest["y_pov"]
 
-    return lopte
+            fig, ax = plt.subplots()
+            ax.set_xlim(0, 20)
+            ax.set_ylim(0, 20)
+            
+            #Crtamo zid
+            ax.plot([x_zid, x_zid], [0,y_zid], color = "black", linewidth = 3)
 
-def animiraj_loptu(lopta):
-    global x_zid, y_zid
-    
-    x, y = lopta.simulacija()
+            #Crtamo pravac y = y0
+            ax.plot([0, x_zid], [y0, y0], color = "red", linestyle = "dashed")
+            putanja, = ax.plot(x[0], y[0], '-o', color = "red", markersize = 5)
 
-    fig, ax = plt.subplots()
-    ax.set_xlim(0, 20)
-    ax.set_ylim(0, 30)
-    #Crtamo zid
-    ax.plot([x_zid, x_zid], [0,y_zid], color = "black", linewidth = 5)
+            def update(frame):
+                novi_x = x[frame]
+                novi_y = y[frame]
+                putanja.set_data(novi_x, novi_y)
 
-    #Crtamo pravac y = y0
-    ax.plot([0, x_zid], [2, 2], color = "red", linestyle = "dashed")
-    putanja, = ax.plot(x[0], y[0], '-o', color = "red", markersize = 5)
-
-    def update(frame):
-        novi_x = x[frame]
-        novi_y = y[frame]
-        putanja.set_data(novi_x, novi_y)
-
-    animation = FuncAnimation(fig, update, frames=range(len(x)), interval=1)
-    plt.show()
+            animation = FuncAnimation(fig, update, frames=range(len(x)), interval=1)
+            plt.show()
 
 
-#Optimalna loptica
-def animacija_optimalne_loptice(theta):
-    v = np.arange(10,40,0.1)
-    d = np.zeros(v.shape)
-    lopte = []
-    for i in range(len(v)):
-        lop = Lopta(x0 = 0, y0 = 2, v0 = v[i], theta0 = theta)
-        lopte.append(lop)
-        x, y = lop.simulacija()
-        d[i] = lop.greska
-    argmin = np.argmin(np.absolute(d))
-    najbolja_lopta = Lopta(x0 = 0, y0 = 2, v0 = v[argmin], theta0 = theta)
-    animiraj_loptu(najbolja_lopta)
 
 
 #Inicijaliziramo početne parametre
-delta_t = 0.025
+delta_t = 0.04
 x_zid, y_zid = 15, 19
+y0 = 2
+theta0 = 45
 k = 0.7
 os.system("clear")
-animacija_vise_loptica()
+
+lopta = Lopta(x0 = 0, y0 = y0, v0 = 63.5, theta0 = 70)
+lopta.simulacija(animiraj = True)
 
 
 
@@ -192,24 +128,3 @@ animacija_vise_loptica()
 
 
 
-
-'''
-
-fig, ax = plt.subplots()
-ax.set_xlim(0, 10)
-ax.set_ylim(0, 10)
-#Crtamo zid
-ax.plot([x_zid, x_zid], [0,y_zid], color = "black", linewidth = 5)
-
-#Crtamo pravac y = y0
-ax.plot([0, x_zid], [2, 2], color = "red", linestyle = "dashed")
-putanja, = ax.plot(x[0], y[0], '-o', color = "red", markersize = 5)
-
-def update(frame):
-    novi_x = x[frame]
-    novi_y = y[frame]
-    putanja.set_data(novi_x, novi_y)
-
-animation = FuncAnimation(fig, update, frames=range(len(x)), interval=1)
-plt.show()
-'''
